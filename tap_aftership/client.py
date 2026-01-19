1d9c2561c727e037b8d939389121ecb81059501f
atttiwari <atul.tiwari@qlik.com>
2026-01-19 13:10:13 +0530

    SAC-29075: Addressed review comments

--- tap_aftership/client.py
+++ tap_aftership/client.py
@@ -102,14 +102,10 @@ class Client:
             self.request_timeout = REQUEST_TIMEOUT
 
     def __enter__(self):
-        self.check_api_credentials()
         return self
 
     def __exit__(self, exception_type, exception_value, traceback):
         self._session.close()
-
-    def check_api_credentials(self) -> None:
-        pass
 
     def authenticate(self, headers: Dict, params: Dict) -> Tuple[Dict, Dict]:
         """Authenticates the request with the token"""
